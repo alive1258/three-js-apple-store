a323430031767248ab844431937e2d26bfdddf2d
zamirul <javshazib@gmail.com>
2025-12-04 09:29:04 +0600

    project setuo done and customr taiwind css add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      {" "}
+      <h1 class="text-3xl font-bold underline text-red-800">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
