9961e3c8239d280e5af178a7b9e005656f897dcd
zamirul <javshazib@gmail.com>
2025-12-06 19:14:48 +0600

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,3 @@
-const navLinks = [
-  { label: "Store" },
-  { label: "Mac" },
-  { label: "iPhone" },
-  { label: "Watch" },
-  { label: "Vision" },
-  { label: "AirPods" },
-];
-
 const noChangeParts = [
   "Object_84",
   "Object_37",
@@ -132,7 +123,6 @@ export {
   features,
   featureSequence,
   footerLinks,
-  navLinks,
   noChangeParts,
   performanceImages,
   performanceImgPositions,
